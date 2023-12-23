9c85b81c0937069039abe88eaa5602e7e53058ec
ops <somewhere@example.com>
2023-12-23 19:22:13 +0930

    Add initial backend, based on https://blog.logrocket.com/getting-started-postgres-react-app/

---
+++ backend/biometricsModel.js
@@ -0,0 +1,121 @@
+require("dotenv").config();
+
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: env.DB_USER,
+  host: env.DB_HOST,
+  database: env.DB_NAME,
+  password: env.DB_PASS,
+  port: 5432,
+});
+
+// get all bp and weight data
+const getBPAndWeight = async () => {
+  try {
+    return await new Promise(function (resolve, reject) {
+      pool.query("SELECT * FROM bp_and_weight", (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        if (results && results.rows) {
+          resolve(results.rows);
+        } else {
+          reject(new Error("No results found"));
+        }
+      });
+    });
+  } catch (error_1) {
+    console.error(error_1);
+    throw new Error("Internal server error");
+  }
+};
+
+// create bp and weight data
+const createBPAndWeight = async (body) => {
+  return new Promise(function (resolve, reject) {
+    const { name, email } = body;
+    pool.query(
+      "INSERT INTO bp_and_weight (date, time, sys, dia, bp, weight_total, weight_fat, weight_muscle, comment) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+      [
+        date,
+        time,
+        sys,
+        dia,
+        bp,
+        weight_total,
+        weight_fat,
+        weight_muscle,
+        comment,
+      ],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        if (results && results.rows) {
+          resolve(
+            `A new BP and Weight record has been added: ${
+              JSON.stringify(results.rows[0])
+            }`,
+          );
+        } else {
+          reject(new Error("No results found"));
+        }
+      },
+    );
+  });
+};
+
+// delete bp and weight data
+const deleteBPAndWeight = async (id) => {
+  return new Promise(function (resolve, reject) {
+    pool.query(
+      "DELETE FROM bp_and_weight WHERE id = $1",
+      [id],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(`BP and Weight record deleted with ID: ${id}`);
+      },
+    );
+  });
+};
+
+// update bp and weight data
+const updateBPAndWeight = (id, body) => {
+  return new Promise(function (resolve, reject) {
+    const { name, email } = body;
+    pool.query(
+      "UPDATE bp_and_weight SET date = $2, time = $3, sys = $4, dia = $5, bp = $6, weight_total = $7, weight_fat = $8, weight_muscle = $9, comment = $10 WHERE id = $1 RETURNING *",
+      [
+        id,
+        date,
+        time,
+        sys,
+        dia,
+        bp,
+        weight_total,
+        weight_fat,
+        weight_muscle,
+        comment,
+      ],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        if (results && results.rows) {
+          resolve(`Merchant updated: ${JSON.stringify(results.rows[0])}`);
+        } else {
+          reject(new Error("No results found"));
+        }
+      },
+    );
+  });
+};
+
+module.exports = {
+  getBPAndWeight,
+  createBPAndWeight,
+  deleteBPAndWeight,
+  updateBPAndWeight,
+};
---
+++ backend/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+
+const biometrics_model = require("./biometricsModel");
+
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers",
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  biometrics_model.getBPAndWeights()
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.post("/biometricss", (req, res) => {
+  biometrics_model.createBPAndWeight(req.body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.delete("/biometricss/:id", (req, res) => {
+  biometrics_model.deleteBPAndWeight(req.params.id)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+app.put("/biometricss/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  biometrics_model
+    .updateBPAndWeight(id, body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
