e60a36fbda75a235243df8e49fe35aaaf6015121
ops <somewhere@example.com>
2024-01-01 20:06:55 +0930

    got the data to display - now trying to add it

--- golang_server/go.mod
+++ golang_server/go.mod
@@ -3,3 +3,5 @@ module main
 go 1.18
 
 require github.com/lib/pq v1.10.9
+
+require github.com/gorilla/schema v1.2.1
--- golang_server/main.go
+++ golang_server/main.go
@@ -6,11 +6,14 @@ import (
 	"log"
 	"net/http"
 	"time"
+  "github.com/gorilla/schema"
+
 )
+
 // using pointers in the data structure to support null values
 // assigned from the database query results.
 type Biometric struct {
-  Id            int
+	Id            int
 	Date          time.Time
 	Time          *time.Time
 	Sys           *int
@@ -25,52 +28,85 @@ type Biometric struct {
 func main() {
 	fmt.Println("Go Biometrics app...")
 
-  err := OpenDatabase()
+	err := OpenDatabase()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-  getAllWeightAndBPData := func() []Biometric {
-    var biometrics []Biometric
-    rows, err := DB.Query("SELECT id, date, time, sys, dia, bp, weight_total, weight_fat, weight_muscle, comment FROM bp_and_weight ORDER BY date DESC, time DESC")
+	getAllBiometricData := func() ([]Biometric, error) {
+		var biometrics []Biometric
+		rows, err := DB.Query("SELECT id, date, time, sys, dia, bp, weight_total, weight_fat, weight_muscle, comment FROM bp_and_weight ORDER BY date DESC, time DESC")
 
+		if err != nil {
+      log.Print("error retrieving biometrics data from database: %v", err)
+      return nil, err
+		}
 
+		defer rows.Close()
+
+		for rows.Next() {
+
+			var biometric Biometric
+
+			err = rows.Scan(&biometric.Id, &biometric.Date, &biometric.Time, &biometric.Sys, &biometric.Dia, &biometric.Bp, &biometric.Weight_total, &biometric.Weight_fat, &biometric.Weight_muscle, &biometric.Comment)
+			if err != nil {
+        log.Print("error assigning biometrics data from database: %v", err)
+        return nil, err
+			}
+
+			biometrics = append(biometrics, biometric)
+		}
+
+		return biometrics, nil
+	}
+
+  addBiometricData := func(biometric Biometric) error {
+    _, err := DB.Exec("INSERT INTO bp_and_weight (date, time, sys, dia, bp, weight_total, weight_fat, weight_muscle, comment) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)", biometric.Date, biometric.Time, biometric.Sys, biometric.Dia, biometric.Bp, biometric.Weight_total, biometric.Weight_fat, biometric.Weight_muscle, biometric.Comment)
     if err != nil {
       log.Fatal(err)
     }
-
-    defer rows.Close()
-
-    for rows.Next() {
-
-      var biometric Biometric
-
-      err = rows.Scan(&biometric.Id, &biometric.Date, &biometric.Time, &biometric.Sys, &biometric.Dia, &biometric.Bp, &biometric.Weight_total, &biometric.Weight_fat, &biometric.Weight_muscle, &biometric.Comment)
-      if err != nil {
-        log.Fatal(err)
-      }
-
-      biometrics = append(biometrics, biometric)
-    }
-
-    return biometrics
+    return nil
   }
 
-
-  handleMainPage := func(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("test.html"))
-    biometrics := getAllWeightAndBPData()
-
-    data := map[string][]Biometric{
-      "Biometrics": biometrics,
+  handleAddBiometric := func(w http.ResponseWriter, r *http.Request) {
+    err := r.ParseForm()
+    if err != nil {
+      http.Error(w, "Failed to parse form values", http.StatusInternalServerError)
+      return
     }
 
-    tmpl.Execute(w, data)
+    var biometric Biometric
+    decoder := schema.NewDecoder()
+
+    err = decoder.Decode(&biometric, r.PostForm)
+
+    if err != nil {
+      http.Error(w, "Failed to decode form values", http.StatusInternalServerError)
+      return
+    }
+
+    err = addBiometricData(biometric)
+    if err != nil {
+      http.Error(w, "Failed to add biometric data", http.StatusInternalServerError)
+      return
+    }
+
   }
 
+	handleMainPage := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("test.html"))
+		biometrics := getAllBiometricData()
+
+		data := map[string][]Biometric{
+			"Biometrics": biometrics,
+		}
+
+		tmpl.Execute(w, data)
+	}
 
 	// define handlers
 	http.HandleFunc("/", handleMainPage)
+	http.HandleFunc("/add-biometric", handleAddBiometric)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 
